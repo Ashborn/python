49b9e9d4b39618aaaec76e8df224dbf8baa68ba0
Ashborn <61397789+Ashborn@users.noreply.github.com>
2021-08-31 19:11:05 +0700

    Add files via upload

---
+++ Variables and Data Types.py
@@ -0,0 +1,9 @@
+
+character_name = "John"
+character_age = 50
+is_male = True
+print('There once was a man named ' + character_name + ', ')
+print('He was '+ character_age +' years old.')
+character_name = "Mike"
+print('He really liked the name ' + character_name + ', ')
+print('But he didnt like being ' + character_age + '.')
